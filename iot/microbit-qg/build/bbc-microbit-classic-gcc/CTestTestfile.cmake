3f0b18dbce08351afcaaa31c87ed321f7bafe68f
vincentgeaycpe <vincent.geay@cpe.fr>
2025-12-11 12:24:51 +0100

    Gitignore

--- iot/microbit-qg/build/bbc-microbit-classic-gcc/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/microbit-samples/build/bbc-microbit-classic-gcc
-# Build directory: /workspaces/microbit-samples/build/bbc-microbit-classic-gcc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("ym/microbit")
-subdirs("source")
--- iot/microbit-qg/build/bbc-microbit-classic-gcc/ym/microbit-dal/source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/microbit-samples/yotta_modules/microbit-dal/source
-# Build directory: /workspaces/microbit-samples/build/bbc-microbit-classic-gcc/ym/microbit-dal/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
