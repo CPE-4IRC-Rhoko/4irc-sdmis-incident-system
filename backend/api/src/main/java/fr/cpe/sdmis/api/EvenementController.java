fed613d4bf37fa1bd88a6ceff1343411c6deb2d6
Hugo-Rodrigues-Dev <hugo.rdg.dev@gmail.com>
2026-01-07 16:47:46 +0100

    Comme dev

--- backend/api/src/main/java/fr/cpe/sdmis/api/EvenementController.java
+++ backend/api/src/main/java/fr/cpe/sdmis/api/EvenementController.java
@@ -4,7 +4,6 @@ import fr.cpe.sdmis.dto.EvenementCreateRequest;
 import fr.cpe.sdmis.dto.EvenementResponse;
 import fr.cpe.sdmis.service.EvenementService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api/evenements")
 public class EvenementController {
     private final EvenementService evenementService;
