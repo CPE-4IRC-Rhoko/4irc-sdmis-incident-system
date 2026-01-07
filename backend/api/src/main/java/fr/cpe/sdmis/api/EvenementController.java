d8629dbb85d60695adae085d6a559dbfcac1e037
Hugo-Rodrigues-Dev <hugo.rdg.dev@gmail.com>
2026-01-07 08:15:34 +0100

    Ajout d'évent et liaison API

--- backend/api/src/main/java/fr/cpe/sdmis/api/EvenementController.java
+++ backend/api/src/main/java/fr/cpe/sdmis/api/EvenementController.java
@@ -4,6 +4,7 @@ import fr.cpe.sdmis.dto.EvenementCreateRequest;
 import fr.cpe.sdmis.dto.EvenementResponse;
 import fr.cpe.sdmis.service.EvenementService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/evenements")
 public class EvenementController {
     private final EvenementService evenementService;
