3e288a93eb3300b8ce6d5354cafe192a142c5fc8
quentin.bouchot <quentin.bouchot@cpe.fr>
2026-01-13 12:19:45 +0100

    remove: useless class in decision engine

--- backend/MoteurDecision/src/main/java/org/example/moteurdecision/domain/status/StatutAgent.java
+++
@@ -1,9 +0,0 @@
-package org.example.moteurdecision.domain.status;
-
-import java.util.UUID;
-
-public class StatutAgent extends AbstractIStatus {
-    public StatutAgent(UUID idStatut, String nomStatut) {
-        super(idStatut, nomStatut);
-    }
-}
--- backend/MoteurDecision/src/main/java/org/example/moteurdecision/domain/status/StatutEvenement.java
+++
@@ -1,9 +0,0 @@
-package org.example.moteurdecision.domain.status;
-
-import java.util.UUID;
-
-public class StatutEvenement extends AbstractIStatus {
-    public StatutEvenement(UUID idStatut, String nomStatut) {
-        super(idStatut, nomStatut);
-    }
-}
